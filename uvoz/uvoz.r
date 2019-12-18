aca396ccc3919a138452c1906fbed45fa1249175
ÄLara Vidmar <lara.snupi@gmail.com>
2019-12-18 20:40:24 +0100

    Uvoz tabel in sprememba v projekt.Rmd

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -2,6 +2,8 @@
 library(readr)
 library(dplyr)
 library(tidyr)
+library(rvest)
+library(gsubfn)
 
 #sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
@@ -32,29 +34,49 @@ uvozi.obcine <- function() {
   return(tabela)
 }
 
-#potniki_zelezniski_promet_evropa <- read_html('file:///C:/Users/VidmarL17/Downloads/rail_pa_total.html')
+# 1. tabela  
 
 letalski_potniski_in_blagovni_promet <- read_csv2("podatki/letalski_potniški_in_blagovni_promet.csv", skip=2,
                     locale=locale(encoding="CP1250")) %>% separate(MESEC, sep="M", c("Leto", "Mesec")) %>%
     mutate(Leto=parse_number(Leto), Mesec=parse_number(Mesec))
+
 letalski_potniski_in_blagovni_promet <- letalski_potniski_in_blagovni_promet[,-c(4,5)] 
 
-#zelezniski_potniski_promet <- read_csv2("podatki/2221702.csv",locale=locale(encoding="CP1250"))
+# 2. tabela 
 
 zelezniski_potniski_promet <- read_csv2("podatki/zelezniski_potniski_promet.csv", skip = 1, locale=locale(encoding="CP1250")) 
 zelezniski_potniski_promet <- zelezniski_potniski_promet[,-c(4)]
 
 zelezniski_blagovni_promet <- read_csv2('podatki/zelezniski_blagovni_promet.csv', skip= 1,locale=locale(encoding="CP1250"))
 
+zelezniski_promet <- inner_join(zelezniski_blagovni_promet, zelezniski_potniski_promet)[, -c(4)] # %>% rename(TRANSPORT = NOTRANJI / MEDNARODNI PREVOZ)
+
+#nova  <- filter(zelezniski_promet, `NOTRANJI / MEDNARODNI PREVOZ`== 'Mednarodni prevoz - potniki vstopili v Sloveniji')
+
+# 3. tabela 
 
 cestni_blagovni_promet <- read_csv2('podatki/cestni_blagovni_promet.csv', skip= 1,locale=locale(encoding="CP1250"))
 cestni_blagovni_promet <- cestni_blagovni_promet[,-c(4, 5)]
+
+# 4. tabela 
+
 pristaniski_potniski_promet <- read_csv2 ('podatki/pristaniski_potniski_promet.csv', skip = 1, locale=locale(encoding="CP1250"), na = '0') 
 
 pristaniski_blagovni_promet <- read_csv2('podatki/pristaniski_blagovni_promet.csv', skip = 1, locale = locale(encoding="CP1250"), na = '0')
+#pristaniski_promet <- inner_join(pristaniski_blagovni_promet, pristaniski_potniski_promet)
 
+# 5. tabela 
+#lin <- 'file:///C:/Users/Lara/Downloads/rail_pa_total%20(1).html'
+#stran <- html_session(lin) %>% read_html()
+#tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+#  .[[1]] %>% html_table(dec=",")
 
+evropski_potniski_promet <- read_csv('podatki/evropa_ljudje.csv' ,  locale=locale(encoding="CP1250"), na = ':') %>% select('TIME', 'GEO', 'Value') %>% rename(Leto = TIME, Države = GEO, Potniki = Value )
 
+evropski_blagovni_promet <- read_csv('podatki/evropa_dobrine.csv' ,  locale=locale(encoding="CP1250"), na = ':') %>% select('TIME', 'GEO', 'Value') %>% rename(Leto = TIME, Države = GEO, Blago = Value )
+
+evropa_promet <- inner_join(evropski_blagovni_promet, evropski_potniski_promet)
+#filter(evropa_promet, Blago != 'NA', Potniki != 'NA' )
 
 # Zapišimo podatke v razpredelnico obcine
 #obcine <- uvozi.obcine()
