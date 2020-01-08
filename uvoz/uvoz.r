bcbd510fd8f74034fe097ba65833b7ed098453ae
ÄLara Vidmar <lara.snupi@gmail.com>
2020-01-08 20:11:25 +0100

    Risanje grafov (vizualizavija)

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -40,55 +40,95 @@ letalski_potniski_in_blagovni_promet <- read_csv2("podatki/letalski_potniški_in
                     locale=locale(encoding="CP1250")) %>% separate(MESEC, sep="M", c("Leto", "Mesec")) %>%
     mutate(Leto=parse_number(Leto), Mesec=parse_number(Mesec))
 
-letalski_potniski_in_blagovni_promet <- letalski_potniski_in_blagovni_promet[,-c(4,5)] 
+letalski_potniski_in_blagovni_promet <- letalski_potniski_in_blagovni_promet[,-c(4,5)] %>%
+  rename('TRANSPORT' = `PRIHOD/ODHOD LETAL`) 
 
+# ločili uvoz in izvoz ter združili po letih
+
+letalski_promet_uvoz <- filter( letalski_potniski_in_blagovni_promet, TRANSPORT == 'Prihod letal' ) %>%
+  group_by(Leto) %>% summarise(sum(Tone), sum(Potniki)) %>% 
+  rename('Tone' ='sum(Tone)', 'Potniki' = 'sum(Potniki)')
+
+
+letalski_promet_izvoz <- filter( letalski_potniski_in_blagovni_promet, TRANSPORT == 'Odhod letal' ) %>%
+  group_by(Leto) %>% summarise(sum(Tone), sum(Potniki)) %>% 
+  rename('Tone' ='sum(Tone)', 'Potniki' = 'sum(Potniki)')
+  
 # 2. tabela 
 
 prevedba <- c("Mednarodni prevoz - blago naloženo v Sloveniji"="izvoz",
               "Mednarodni prevoz - blago razloženo v Sloveniji"="uvoz")
+prevedba2 <- c('Mednarodni prevoz - potniki vstopili v Sloveniji' = 'uvoz',
+              'Mednarodni prevoz - potniki izstopili v Sloveniji' = 'izvoz'	)
 
 zelezniski_potniski_promet <- read_csv2("podatki/zelezniski_potniski_promet.csv", skip = 1, locale=locale(encoding="CP1250")) 
-zelezniski_potniski_promet <- zelezniski_potniski_promet[,-c(4)]
 
-zelezniski_blagovni_promet <- read_csv2('podatki/zelezniski_blagovni_promet.csv', skip= 1,locale=locale(encoding="CP1250"))
+zelezniski_potniski_promet <- zelezniski_potniski_promet[,-c(4)] %>%
+  rename("TRANSPORT" = "NOTRANJI/MEDNARODNI PREVOZ") %>%
+  mutate(TRANSPORT=prevedba2[TRANSPORT]) 
+  
+zelezniski_uvoz1 <-  filter(zelezniski_potniski_promet, TRANSPORT == 'uvoz') 
+zelezniski_uvoz2 <-  filter(zelezniski_blagovni_promet, TRANSPORT == 'uvoz')
+zelezniski_uvoz <- inner_join(zelezniski_uvoz1, zelezniski_uvoz2)
+  
+zelezniski_izvoz1 <-  filter(zelezniski_potniski_promet, TRANSPORT == 'izvoz') 
+zelezniski_izvoz2 <-  filter(zelezniski_blagovni_promet, TRANSPORT == 'izvoz')
+zelezniski_izvoz <- inner_join(zelezniski_izvoz1, zelezniski_izvoz2)
 
-zelezniski_promet <- inner_join(zelezniski_blagovni_promet, zelezniski_potniski_promet)[, -c(4)]%>%
+zelezniski_blagovni_promet <- read_csv2('podatki/zelezniski_blagovni_promet.csv', skip= 1,locale=locale(encoding="CP1250")) %>%
   rename("TRANSPORT" = "NOTRANJI / MEDNARODNI PREVOZ") %>%
   mutate(TRANSPORT=prevedba[TRANSPORT])
 
-#nova  <- filter(zelezniski_promet, `NOTRANJI / MEDNARODNI PREVOZ`== 'Mednarodni prevoz - potniki vstopili v Sloveniji')
+
 
 # 3. tabela 
 
-cestni_blagovni_promet <- read_csv2('podatki/cestni_blagovni_promet.csv', skip= 1,locale=locale(encoding="CP1250"))
-cestni_blagovni_promet <- cestni_blagovni_promet[,-c(4, 5)]
+prevedba5 <- c('Mednarodni prevoz - blago naloženo v Sloveniji' = 'izvoz',
+               'Mednarodni prevoz - blago razloženo v Sloveniji' = 'uvoz')
 
-# 4. tabela 
+cestni_blagovni_promet <- read_csv2('podatki/cestni_blagovni_promet.csv', skip= 1,locale=locale(encoding="CP1250"))
+cestni_blagovni_promet <- cestni_blagovni_promet[,-c(4, 5)] %>% 
+  mutate(`NOTRANJI / MEDNARODNI PREVOZ` = prevedba5[`NOTRANJI / MEDNARODNI PREVOZ`])
+
+# 4. tabela
+
+prevedba3 <- c('Odpotovali potniki' = 'izvoz', 'Prispeli potniki' = 'uvoz')
+prevedba4 <- c('Razloženi tovor, uvoz' = 'uvoz','Naloženi tovor, izvoz' = 'izvoz')
 
 pristaniski_potniski_promet <- read_csv2('podatki/pristaniski_potniski_promet.csv',
                                          skip = 1, locale=locale(encoding="CP1250"), na = '0') %>%
-  rename(potniki=`PROMET POTNIKOV`) %>%
-  gather(key="leto.mesec", value="stevilo", -potniki) %>%
+  rename(transport=`PROMET POTNIKOV`) %>%
+  gather(key="leto.mesec", value="stevilo", -transport) %>%
   separate(leto.mesec, c("leto", "mesec"), "M") %>%
-  mutate(leto=parse_number(leto), mesec=parse_number(mesec))
+  mutate(leto=parse_number(leto), mesec=parse_number(mesec)) %>%
+  group_by(leto, transport) %>% summarise(Potniki = sum(stevilo)) %>%
+  mutate(transport = prevedba3[transport]) %>%
+  arrange(transport)
+  
 
-pristaniski_blagovni_promet <- read_csv2('podatki/pristaniski_blagovni_promet.csv', skip = 1, locale = locale(encoding="CP1250"), na = '0')
-#pristaniski_promet <- inner_join(pristaniski_blagovni_promet, pristaniski_potniski_promet)
+pristaniski_blagovni_promet <- read_csv2('podatki/pristaniski_blagovni_promet.csv', skip = 1, locale = locale(encoding="CP1250"), na = '0') %>%
+  mutate(`RAZLOŽENI IN NALOŽENI TOVOR` = prevedba4[`RAZLOŽENI IN NALOŽENI TOVOR`]) %>%
+  arrange(`RAZLOŽENI IN NALOŽENI TOVOR`)
 
+#pristaniski <- inner_join(pristaniski_blagovni_promet, pristaniski_potniski_promet)
+
+  
 # 5. tabela 
 #lin <- 'file:///C:/Users/Lara/Downloads/rail_pa_total%20(1).html'
 #stran <- html_session(lin) %>% read_html()
 #tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
 #  .[[1]] %>% html_table(dec=",")
 
-evropski_potniski_promet <- read_csv('podatki/evropa_ljudje.csv' ,  locale=locale(encoding="CP1250"), na = ':') %>% select('TIME', 'GEO', 'Value') %>% rename(Leto = TIME, Države = GEO, Potniki = Value )
+evropski_potniski_promet <- read_csv('podatki/evropa_ljudje.csv' ,  locale=locale(encoding="CP1250"), na = ':') %>%
+  select('TIME', 'GEO', 'Value') %>% 
+  rename(Leto = TIME, Države = GEO, Potniki = Value )
 
-evropski_blagovni_promet <- read_csv('podatki/evropa_dobrine.csv' ,  locale=locale(encoding="CP1250"), na = ':') %>% select('TIME', 'GEO', 'Value') %>% rename(Leto = TIME, Države = GEO, Blago = Value )
+evropski_blagovni_promet <- read_csv('podatki/evropa_dobrine.csv' ,  locale=locale(encoding="CP1250"), na = ':') %>% 
+  select('TIME', 'GEO', 'Value') %>% 
+  rename(Leto = TIME, Države = GEO, Blago = Value )
 
-evropa_promet <- inner_join(evropski_blagovni_promet, evropski_potniski_promet)
-
-#filter(evropa_promet, Blago != 'NA', Potniki != 'NA' )
-
+evropa_promet <- inner_join(evropski_blagovni_promet, evropski_potniski_promet) 
+evropa_promet <- na.omit(evropa_promet)
 # Zapišimo podatke v razpredelnico obcine
 #obcine <- uvozi.obcine()
 
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,81 @@
 # 3. faza: Vizualizacija podatkov
 
+library(ggplot2)
+library(dplyr)
+library(tmap)
+library(rgdal)
+library(rgeos)
+library(ggpubr)
+
+
+
 # Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-                             pot.zemljevida="OB", encoding="Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
-zemljevid <- fortify(zemljevid)
+#zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
+#                             pot.zemljevida="OB", encoding="Windows-1250")
+#levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
+#  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
+#zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
+#zemljevid <- fortify(zemljevid)
 
 # Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+#povprecja <- druzine %>% group_by(obcina) %>%
+#  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+
+# stolpični graf POVPREČNI TRANSPORT V 9 LETIH PO DRŽAVAH 
+povprecje_blaga <- evropa_promet  %>% group_by(Države) %>%
+  summarise(`povprecje blaga` = sum(Blago)/9)
+
+povprecje_potnikov <- evropa_promet  %>% group_by(Države) %>%
+  summarise(`povprecje potnikov` = sum(Potniki)/9)
+
+graf_evropa_blago <- ggplot(data = povprecje_blaga, mapping = aes(x= Države, y= `povprecje blaga`)) +
+  geom_bar(stat = 'identity', position = 'dodge') +
+  theme(axis.text.x = element_text(angle = 90, size = 8)) +
+  ggtitle('Povprečni transport blaga v 9 letih po državah') 
+
+graf_evropa_potniki <- ggplot(data = povprecje_potnikov, mapping = aes(x= Države, y= `povprecje potnikov`)) +
+  geom_bar(stat = 'identity', position = 'dodge') +
+  theme(axis.text.x = element_text(angle = 90, size = 8)) +
+  ggtitle('Povprečni transport potnikov v 9 letih po državah')
+
+graf_evropa <- ggarrange(graf_evropa_blago, graf_evropa_potniki) 
+
+
+
+
+# linijski grafi za UVOZ BLAGA
+graf_let_uvoz_blaga <- ggplot(data = letalski_promet_uvoz, mapping = aes(x= Leto, y = Tone))+
+  geom_line(stat = 'identity', col ='red') +
+  ggtitle("Letalski promet uvoz blaga")
+
+
+cestni_blagovni_promet_uvoz <- filter(cestni_blagovni_promet,`NOTRANJI / MEDNARODNI PREVOZ` == 'uvoz')
+
+graf_ces_uvoz_blaga <- ggplot(data = cestni_blagovni_promet_uvoz, mapping = aes(x = cestni_blagovni_promet_uvoz$LETO, y = `Tone (1000)`)) +
+  geom_line(stat = 'identity', col ='green') +
+  ggtitle("Cestni promet uvoz blaga")
+
+
+
+
+graf_zel_uvoz_blaga <- ggplot(data = zelezniski_uvoz, mapping = aes(x=zelezniski_uvoz$LETO , y=zelezniski_uvoz$`Tone (1000)`)) + 
+  geom_line(stat = 'identity', col= 'blue') +
+  ggtitle("Železniški promet uvoz blaga")
+print(graf_zel_uvoz_blaga)
+ 
+pristaniski_blagovni_promet <- filter(pristaniski_blagovni_promet, 'RAZLOŽENI IN NALOŽENI TOVOR' == 'uvoz') 
+
+graf_pris_uvoz_blago <- ggplot(date= pristaniski_blagovni_promet, mapping = aes(x= pristaniski_blagovni_promet$LETO, y = pristaniski_blagovni_promet$`Tone [1000]`))+
+  geom_line(stat = 'identity', col ='yellow') +
+  ggtitle("Pristaniški promet uvoz blaga")
+
+print(graf_pris_uvoz_blago)
+
+
+
+#linijski grafi ZA IZVOZ BLAGA
+x <- ggarrange(graf_let_uvoz, promet2) #dobimo na istem listu oba grafa 
+
+
+
+
